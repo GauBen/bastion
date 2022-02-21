f6455609c03dc0ba7028edc204e0ccdb66759abb
GauBen <benaim.gautier@gmail.com>
2022-02-21 12:39:35 +0100

    feat: beautiful error page

---
+++ apps/frontend/src/routes/__error.svelte
@@ -0,0 +1,27 @@
+<script lang="ts" context="module">
+  import type { ErrorLoad } from '@sveltejs/kit'
+
+  export const load: ErrorLoad = ({ error, status }) => {
+    return {
+      props: {
+        title: `${status}: ${error?.message}`,
+      },
+    }
+  }
+</script>
+
+<script lang="ts">
+  export let title: string
+</script>
+
+<h1>{title}</h1>
+
+<style lang="scss">
+  :global(#app) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    background-color: $background;
+  }
+</style>
