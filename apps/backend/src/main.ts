f18f24af9835ea0ad25d781cf20bf63c1afaed22
GauBen <benaim.gautier@gmail.com>
2022-02-25 13:54:06 +0100

    fix: properly load env vars

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
+import { config as dotenv } from 'dotenv'
 import { AppModule } from './app.module'
 
 export const bootstrap = async () => {
+  dotenv({ path: `${__dirname}/../../../.env` })
   const app = await NestFactory.create(AppModule)
   app.use(cookieParser())
   app.useGlobalPipes(new ValidationPipe())
