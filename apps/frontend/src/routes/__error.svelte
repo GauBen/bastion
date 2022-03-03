1ebe3eaf08bf1c4825021e14bf73047a33afd3b7
GauBen <benaim.gautier@gmail.com>
2022-03-03 15:15:40 +0100

    feat: improved error page

--- apps/frontend/src/routes/__error.svelte
+++ apps/frontend/src/routes/__error.svelte
@@ -2,28 +2,42 @@
   import type { ErrorLoad } from '@sveltejs/kit'
 
   export const load: ErrorLoad = ({ error, status }) => {
+    console.log(error, status)
     return {
-      props: {
-        title: `${status}: ${error?.message}`,
-      },
+      stuff: { title: 'Error' },
+      props: { error, status },
     }
   }
 </script>
 
 <script lang="ts">
-  export let title: string
+  export let error: Error | undefined
+  export let status: number | undefined
 </script>
 
 <main>
-  <h1>{title}</h1>
+  <div>
+    <h1>{status}</h1>
+    <p>{error?.message}</p>
+  </div>
 </main>
 
 <style lang="scss">
   main {
     display: flex;
-    align-items: center;
     justify-content: center;
+    align-items: center;
     min-height: 100vh;
     background-color: $background;
+  }
+
+  div {
+    padding: 1em 2em;
+    background-color: $light-background;
+    border-radius: 1em;
+    box-shadow: 0 1em 2em $shadow;
+    width: 400px;
+    max-width: 100%;
+    margin: 0.5em;
   }
 </style>
